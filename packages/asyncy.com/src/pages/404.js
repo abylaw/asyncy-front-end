8c66b86ed8beb0ec21e85a5d1001187f7688a261
Andrea Law <abylaw@gmail.com>
2018-04-19 11:39:41 -0400

    404 page

--- packages/asyncy.com/src/pages/404.js
+++ packages/asyncy.com/src/pages/404.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Link from 'gatsby-link'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <section className='section' style={{
+    textAlign: 'center',
+  }}>
+    <h1>404</h1>
+    <h3>Oops. The page you were looking for doesn't exist.</h3>
+    <p>You may have mistyped the address or the page may have moved</p>
+
+    <Link to='/'><button className='secondary'>Go to homepage</button></Link>
+  </section>
 )
 
 export default NotFoundPage
