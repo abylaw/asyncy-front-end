e1ce9e7c2771ea35275f7c2845ddb3650c2f5160
Andrea Law <abylaw@gmail.com>
2018-05-04 12:20:55 -0400

    Update events

--- packages/asyncy.com/src/pages/events.js
+++ packages/asyncy.com/src/pages/events.js
@@ -3,25 +3,17 @@ import Hero from '../components/Hero'
 
 import calendar from '../assets/images/icon_calendar.svg'
 import pin from '../assets/images/icon_pin.svg'
-import placeholderEventImage from '../assets/images/placeholder_event_image.svg'
+import launchPartyImg from '../assets/images/launch-party.png'
 
 const data = {
   title: 'Upcoming Events',
   subtitle: 'Connect with the Asyncy community at conferences, meetups, and hackathons around the world.',
   events: [
     {
-      title: 'Asyncy Microhack',
-      description: 'A training and user feedback session on how to rapidly develop applications using a new strategy of choreographed microservices on the Asyncy Platform using Storyscript.',
-      date: 'March 1 2018',
-      image: placeholderEventImage,
-      location: 'Amsterdam',
-      link: '/'
-    },
-    {
       title: 'Asyncy Launch Party',
       description: 'Join us for the official beta launch of Asyncy. Meet the team, launch your first applications and boat around the canals!',
       date: 'Late September 2018',
-      image: placeholderEventImage,
+      image: launchPartyImg,
       location: 'Amsterdam',
       onClick: () => window.typeform.launchparty.open(),
     }
@@ -51,7 +43,9 @@ const EventPage = () => (
       maxWidth: '1000px',
     }}>
       <div className='columns'>
-        <div className='column is-three-quarters'>
+        <div className='column is-three-quarters' style={{
+          margin: '0 auto',
+        }}>
           {
             data.events.map((event, index) => {
               return <div className='columns' key={index} style={{
@@ -59,20 +53,20 @@ const EventPage = () => (
                 borderBottom: (index === data.events.length - 1) ? 'none' : '1px solid #41434A'
               }}>
                 <div className='column is-narrow'>
-                  <div style={{
-                    height: '145px',
-                    width: '145px',
-                    borderRadius: '4px',
-                  }}>
-                    <img src={event.image}/>
+                  <div>
+                    <img style={{
+                      height: '145px',
+                      width: '145px',
+                      borderRadius: '4px',
+                    }} src={event.image}/>
                   </div>
                 </div>
                 <div className='column'>
                   <h4>{event.title}</h4>
-                  <subtitle style={{
+                  <subtitle className="level" style={{
                     display: 'block',
                     color: '#CCCCD0',
-                    fontSize: '0.8em',
+                    fontSize: '0.9em',
                     margin: '0.8em 0',
                   }}>
                     <span style={{
@@ -97,30 +91,6 @@ const EventPage = () => (
             })
           }
           </div>
-
-        <div className='column'>
-          <h5>Education & Training Events</h5>
-
-          <ul style={{
-            paddingLeft: 0,
-            listStyle: 'none',
-          }}>
-          {
-            data.learningEvents.map((event, index) => (
-              <li key={index} style={{
-                marginBottom: '1em',
-              }}>
-                <a href={event.link} style={{ color: '#00AAFF' }}>
-                  {event.title}
-                </a>
-                <p style={{
-                  fontSize: '0.8em',
-                }}>{event.date} • {event.location}</p>
-              </li>
-            ))
-          }
-          </ul>
-        </div>
       </div>
     </section>
   </div>
