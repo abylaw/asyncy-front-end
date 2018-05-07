355b99add979aa59d7e96571970465874f48a77e
Andrea Law <abylaw@gmail.com>
2018-05-07 14:07:35 -0400

    Clean pageclip code

--- packages/asyncy.com/plugins/gatsby-plugin-pageclip/gatsby-browser.js
+++ packages/asyncy.com/plugins/gatsby-plugin-pageclip/gatsby-browser.js
@@ -1,5 +1,31 @@
 import validate from 'validate.js'
 
+const resetButtonText = (button) => {
+  if (button.dataset.originalText) {
+    button.innerHTML = `<span>${button.dataset.originalText}</span>`
+  } else {
+    button.dataset.originalText = button.textContent;
+  }
+}
+
+const showSuccess = (form, button, successClass, successMsg) => {
+  form.classList.add(successClass);
+  button.innerHTML = `<span>${successMsg}</span>`;
+}
+
+const showError = (form, errorContainer, errorClass, errorMsg) => {
+  form.classList.add(errorClass);
+  errorContainer.textContent = errorMsg;
+}
+
+const formConstraints = {
+  email: {
+    email: {
+      message: 'Please enter a valid email address.'
+    }
+  }
+}
+
 exports.onRouteUpdate = (_, pluginOptions) => {
   const successClass = pluginOptions.successClass;
   const errorClass = pluginOptions.errorClass;
@@ -12,56 +38,36 @@ exports.onRouteUpdate = (_, pluginOptions) => {
     for (const form of forms) {
       window.Pageclip.form(form, {
         onSubmit() {
-          let input = form.getElementsByTagName('input').email;
-          let button = form.getElementsByTagName('button').submit;
-          let errorContainer = form.getElementsByClassName(errorMessageContainerClass)[0];
+          const input = form.getElementsByTagName('input').email;
+          const button = form.getElementsByTagName('button').submit;
+          const errorContainer = form.getElementsByClassName(errorMessageContainerClass)[0];
 
-          const oldInput = button.innerHTML;
-
-          if (button.dataset.originalText) {
-            button.innerHTML = `<span>${button.dataset.originalText}</span>`
-          } else {
-            button.dataset.originalText = button.textContent;
-          }
-
+          resetButtonText(button);
           form.classList.remove(errorClass);
           form.classList.remove(successClass);
           errorContainer.textContent = '';
 
-          const constraints = {
-            email: {
-              email: {
-                message: 'Please enter a valid email address.'
-              }
-            }
-          }
-
-          const hasError = validate({ email: input.value }, constraints);
+          const hasError = validate({ email: input.value }, formConstraints);
 
           if (hasError) {
-            form.classList.add(errorClass);
-            errorContainer.textContent = hasError.email.join(', ').replace('Email ', '');
+            showError(form, errorContainer, errorClass, hasError.email.join(', ').replace('Email ', ''));
             setTimeout(() => {
               button.classList.remove('pageclip-form__submit--loading');
               button.disabled = false;
-            }, 200);
+            }, 300);
             return false;
           } else {
             return true;
           }
         },
         onResponse(error, response) {
-          let errorContainer = form.getElementsByClassName(errorMessageContainerClass)[0];
-
-          let button = form.getElementsByTagName('button');
-          button = button.submit;
+          const errorContainer = form.getElementsByClassName(errorMessageContainerClass)[0];
+          const button = form.getElementsByTagName('button').submit;
 
           if (!error) {
-            form.classList.add(successClass);
-            button.innerHTML = `<span>${successMessage}</span>`;
+            showSuccess(form, button, successClass, successMessage);
           } else {
-            form.classList.add(errorClass);
-            errorContainer.textContent = 'There was an error submitting your data. Please try again.';
+            showError(form, errorContainer, errorClass, 'There was an error submitting your data. Please try again.');
           }
           return false;
         },
